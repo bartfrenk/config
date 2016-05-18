6c5f1b8d25e66c7e03590267921afcc5d59cdaa2
Bart Frenk <bart.frenk@gmail.com>
2016-05-18 15:02:49 +0200

    Get completion to work in Haskell mode emacs

--- .emacs.d/config/settings-company.el
+++ .emacs.d/config/settings-company.el
@@ -3,16 +3,25 @@
 (use-package company
   :commands global-company-mode
   :init
+  (defun define-active-map-keys()
+    (define-key company-active-map "\C-x\M-h" 'company-show-doc-buffer)
+    (define-key company-active-map "\C-n" 'company-select-next)
+    (define-key company-active-map "\C-p" 'company-select-previous)
+    (define-key company-active-map "\C-h" 'delete-backward-char))
+  (add-hook 'company-mode-hook 'define-active-map-keys)
   (global-company-mode)
   (setq company-global-modes '(not sage-mode
                                    message-mode))
+
+  :bind
+  (("M-<tab>" . company-complete))
+
   :config
   (setq company-tooltip-limit 20
-        company-idle-delay .3
+        company-idle-delay .1
         company-echo-delay 0
         company-begin-commands '(self-insert-command)
-        company-backends (delq 'company-capf company-backends))
-  :bind
-  (("M-<tab>" . company-complete)))
+        company-backends (delq 'company-capf company-backends)))
+
 
 (provide 'settings-company)
