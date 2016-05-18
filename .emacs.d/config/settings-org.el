21b45648591836c45681a4a86af4ba50d2726d40
Bart Frenk <bart.frenk@gmail.com>
2016-05-18 15:07:50 +0200

    Enable M-q fill in latex environments org mode

--- .emacs.d/config/settings-org.el
+++ .emacs.d/config/settings-org.el
@@ -1,11 +1,18 @@
 (require 'use-package)
+(require 'org)
+
+(defun org-fill-paragraph--latex-environment (&rest args)
+  "Use default fill-paragraph in latex environments."
+  (not (eql (org-element-type (org-element-context)) 'latex-environment)))
+(advice-add 'org-fill-paragraph :before-while #'org-fill-paragraph--latex-environment)
 
 (use-package org
   :init
+
   (define-key global-map "\C-cl" 'org-store-link)
   (define-key global-map "\C-ca" 'org-agenda)
   :config
-  (setq org-log-done t)
-  (setq org-src-fontify-natively t))
+  (setq org-log-done t
+        org-src-fontify-natively t))
 
 (provide 'settings-org)
