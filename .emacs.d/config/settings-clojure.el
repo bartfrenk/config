fc18b60e40381290273a57aaee65cd2e97ff454d
Bart Frenk <bart.frenk@greenhousegroup.com>
2016-06-21 16:37:00 +0200

    Add functionality to clojure mode

--- .emacs.d/config/settings-clojure.el
+++ .emacs.d/config/settings-clojure.el
@@ -1,3 +1,9 @@
 ;;; TODO: enable paredit and paredit-evil modes
 
 (provide 'settings-clojure)
+
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (paredit-mode)
+            (eldoc-mode)
+            (clj-refactor-mode 1)))
