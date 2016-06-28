a50880b66fadc58d05ac226dc67c8e95e80070e5
Bart Frenk <bart.frenk@greenhousegroup.com>
2016-06-28 17:16:48 +0200

    Add slack to XMonad; Minor changes to clojure and python config emacs

--- .emacs.d/init/init-clojure.el
+++ .emacs.d/init/init-clojure.el
@@ -13,13 +13,32 @@
         nrepl-hide-special-buffers t
         cider-use-overlays nil)
   (add-to-list 'evil-emacs-state-modes 'cider-stacktrace-mode)
+  (add-to-list 'evil-emacs-state-modes 'cider-test-report-mode)
   (add-to-list 'helm-boring-buffer-regexp-list "*nrepl-server.*")
   (add-to-list 'helm-boring-buffer-regexp-list "*nrepl-messages.*")
+
+
   :diminish cider-mode
   :ensure t)
 
 (use-package paredit
   :ensure t)
+
+;; Endy: indentation expect-call etc.
+ ;; '(define-clojure-indent
+ ;;    ;; Core
+ ;;    (send-off 1) (cli 1) (go-loop 1)
+
+ ;;    ;;Compojure
+ ;;    (ANY 2) (GET 2) (POST 2) (PUT 2) (PATCH 2) (DELETE 2)
+ ;;    (OPTIONS 2)
+
+ ;;    ;; expect-call
+ ;;    (expect-call 1)
+ ;;    ;; Korma
+ ;;    ;; (select 1) (insert 1) (update 1) (where 1) (set-fields 1)
+ ;;    ;; (values 1) (delete 1) (upsert 1) (subselect 1)
+ ;;    ))
 
 ;; (use-package helm-clojuredocs
 ;;  :ensure t)
