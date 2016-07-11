64a6e37d026a42e5e36dffb53dc507601534e862
Bart Frenk <bart.frenk@gmail.com>
2016-07-11 22:45:59 +0200

    Enable show-parens mode

--- .emacs.d/init/init-core.el
+++ .emacs.d/init/init-core.el
@@ -9,6 +9,7 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
+(show-paren-mode 1)
 
 (set-frame-font "Source Code Pro 10" nil t)
 (add-to-list 'default-frame-alist '(font . "Source Code Pro 10"))
