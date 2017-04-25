bb265df75b20a152e96caed47ff1ea93e33b3b8d
Bart Frenk <bart.frenk@gmail.com>
2017-04-25 21:27:21 +0200

    Enable Erlang edoc mode

--- .emacs.d/init/init-erlang.el
+++ .emacs.d/init/init-erlang.el
@@ -2,6 +2,10 @@
 
 (use-package erlang
   :ensure t
+  :init
+  (add-hook 'erlang-mode-hook (lambda ()
+                                 (erlang-edoc-mode)))
+
   :config
   (setq erlang-electric-commands nil
         erlang-indent-level 2))
