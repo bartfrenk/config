5113b9cea03b49e15365b6fc8274bfbd7918d057
Bart Frenk <bart.frenk@gmail.com>
2017-06-06 00:38:35 +0200

    Hide leading stars in org mode

--- .emacs.d/init/init-org.el
+++ .emacs.d/init/init-org.el
@@ -51,6 +51,7 @@
         org-completion-use-ido nil
         org-outline-path-complete-in-steps nil
         org-default-notes-file "~/documents/org/organizer.org"
+        org-hide-leading-stars t
         org-todo-keywords '((sequence
                              "TODO(t)" "WAIT(w)" "STARTED(s)"
                              "|" "DONE(d)" "CANCELED(c)"))
