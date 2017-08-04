8f862a70174d2ea7414e7bf821a33267cbce9d70
Bart Frenk <bart.frenk@gmail.com>
2017-08-04 17:56:13 +0200

    Improve Go Emacs config

--- .emacs.d/init/init-go.el
+++ .emacs.d/init/init-go.el
@@ -3,14 +3,18 @@
 
 (use-package go-mode
   :bind (:map go-mode-map
-              ("M-]" . godef-jump))
+              ("M-]" . godef-jump)
+              ("M-[" . pop-tag-mark))
   :ensure t
   :config
   (add-hook 'before-save-hook 'gofmt-before-save)
   (add-hook 'go-mode-hook
             (lambda ()
               (set (make-local-variable 'whitespace-style)
-                   '(face lines-tail trailing)))))
+                   '(face lines-tail trailing))
+              (if (not (string-match "go" compile-command))
+              (set (make-local-variable 'compile-command)
+                   "go build -v && go test -v && go vet")))))
 
 (use-package gorepl-mode
   :ensure t)
