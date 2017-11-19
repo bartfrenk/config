b1ad07996441dc040f87798b8d5087eac71e16e0
Bart Frenk <bart.frenk@gmail.com>
2017-11-19 12:35:20 +0100

    Replace obsolete projectile-global-mode by projectile-mode

--- .emacs.d/init/init-projectile.el
+++ .emacs.d/init/init-projectile.el
@@ -8,7 +8,6 @@
         projectile-enable-caching t
         projectile-cache-file (expand-file-name  "projectile.cache" cache-dir)
         projectile-globally-ignored-file-suffixes '(".png" ".gif" ".pdf"))
-  (bind-key (kbd "C-c p s") 'helm-projectile-ag projectile-mode-map)
   :bind
   ("<f5>" . projectile-compile-project)
   ("<f6>" . next-error))
@@ -21,5 +20,6 @@
   (:map projectile-mode-map
        ("C-c p s" . helm-projectile-ag)))
 
-(projectile-global-mode)
+(projectile-mode)
+
 (provide 'init-projectile)
