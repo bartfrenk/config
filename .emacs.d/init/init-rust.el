60132479ac5c1de1f833113c3f43c8a7f958d987
Bart Frenk <bart.frenk@gmail.com>
2018-02-25 23:01:35 +0100

    Update rust config

--- .emacs.d/init/init-rust.el
+++ .emacs.d/init/init-rust.el
@@ -1,6 +1,16 @@
 (require 'use-package)
 
 (use-package rust-mode
+  :bind (:map rust-mode-map
+              ("C-c C-a" . rust-format-buffer))
+  :config
+  (setq rust-format-on-save t
+        rust-rustfmt-bin "rustfmt-force")
   :ensure t)
+
+(use-package flycheck-rust
+  :ensure t)
+
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 (provide 'init-rust)
