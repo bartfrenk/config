a38e0d0248dd1b950f453b6fd2f5f1cfd1092db6
Bart Frenk <bart.frenk@gmail.com>
2018-06-26 23:52:23 +0200

    Write some functions

--- .emacs.d/init/init-python-pyenv.el
+++ .emacs.d/init/init-python-pyenv.el
@@ -5,8 +5,7 @@
 ; requires yapf to be installed
 
 (defun python-format-buffer ()
-  "Format a Python buffer."
-  (interactive)
+  "Format a Python buffer." (interactive)
   (yapfify-buffer))
 
 (use-package python
@@ -22,8 +21,7 @@
                                 (sphinx-doc-mode)
                                 ;(yapf-mode)
                                 ))
-  (when (executable-find "ipython")
-    (setq python-shell-interpreter "ipython"
+  (when (executable-find "ipython") (setq python-shell-interpreter "ipython"
           python-shell-interpreter-args "--simple-prompt"))
   :config
   (setq python-shell-completion-native-enable nil)
@@ -101,11 +99,59 @@ del sys
 "
   "Python script to run immediately after starting the REPL.")
 
-(defun my-python-shell-dir-setup ()
+(defun python--shell-set-up-dirs ()
   (let ((process (get-buffer-process (current-buffer))))
     (python-shell-send-string set-up-python-repl process)
     (message "Setup project path")))
 
-(add-hook 'inferior-python-mode-hook 'my-python-shell-dir-setup)
+(add-hook 'inferior-python-mode-hook 'python--shell-set-up-dirs)
+
+;; TODO: deal with __init__.py buffers
+(defun python--package-name (path)
+  "Derive the package name for the file specified by PATH."
+  (let* ((segments (reverse (split-string path "/")))
+         (package (list (file-name-base (car segments))))
+         (path (string-remove-suffix (concat "/" (car segments)) path))
+         (segments (cdr segments)))
+    (while (file-exists-p (concat path "/__init__.py") )
+      (setq package (cons (car segments) package)
+            path (string-remove-suffix (concat "/" (car segments)) path )
+            segments (cdr segments)))
+    (string-join package ".")))
+
+(python--package-name "/home/bart/dev/projects/experiments/py-bandit/src/bandit/web/generic.py")
+
+;; TODO: might be better to deal with this via from <package name> import *, and relying on
+;; ipython's autoreload extension.
+(defun python--shell-set-package-name (orig-func &rest args)
+  (let ((process (get-buffer-process (current-buffer)))
+        (snippet (concat "__name__ = \""
+                         (python--package-name buffer-file-name)
+                         "\"")))
+    (python-shell-send-string snippet process))
+  (apply orig-func args))
+
+(advice-add 'python-shell-send-buffer :around #'python--shell-set-package-name)
+
+
+
+
+
+
+
+
+;; (defun python-set-name-to-current-buffer
+;;     (orig-fun &rest args)
+;;   (message buffer-file-name)
+;;   (let ((res (apply orig-fun args)))
+;;     res))
+
+;; (advice-add
+;;  'python-shell-send-buffer
+;;  :around
+;;  #'python-set-name-to-current-buffer)
+
+
+
 
 (provide 'init-python-pyenv)
