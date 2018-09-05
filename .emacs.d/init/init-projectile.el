7d8fba26967e5fc3ad9730e0bc3ddc88e28cf3b9
Bart Frenk <bart.frenk@lemonpi.io>
2018-09-05 16:55:36 +0200

    Turn on helm-projectile key bindings

--- .emacs.d/init/init-projectile.el
+++ .emacs.d/init/init-projectile.el
@@ -1,12 +1,13 @@
 (require 'use-package)
 (require 'constants)
+(require 'helm)
 
 (use-package projectile
   :ensure t
   :config
   (setq projectile-completion-system 'helm
         projectile-enable-caching t
-        projectile-cache-file (expand-file-name  "projectile.cache" cache-dir)
+        projectile-cache-file (expand-file-name "projectile.cache" cache-dir)
         projectile-globally-ignored-file-suffixes '(".png" ".gif" ".pdf"))
   :bind
   ("<f5>" . projectile-compile-project)
@@ -14,13 +15,13 @@
   :diminish projectile-mode)
 
 (use-package helm-projectile
-  :ensure t
+  :commands (helm-projectile-on)
   :config
   (setq projectile-switch-project-action 'helm-projectile)
-  :bind
-  (:map projectile-mode-map
-       ("C-c p s" . helm-projectile-ag)))
+  :bind (:map projectile-mode-map
+              ("C-c p s" . helm-projectile-ag)))
 
 (projectile-mode)
+(helm-projectile-on)
 
 (provide 'init-projectile)
