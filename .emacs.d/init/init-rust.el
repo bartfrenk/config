7639d27a2c11f4f6dd51ecbcfa7aec90684bfbf8
Bart Frenk <bart.frenk@gmail.com>
2018-09-21 15:32:30 +0200

    Update Rust config Emacs

--- .emacs.d/init/init-rust.el
+++ .emacs.d/init/init-rust.el
@@ -1,12 +1,21 @@
 (require 'use-package)
+(require 'helm-dash)
+
+(defun rust/setup-dash ()
+  (setq-local helm-dash-docsets '("Rust")))
 
 (use-package rust-mode
   :bind (:map rust-mode-map
-              ("C-c C-a" . rust-format-buffer))
+              ("C-c C-a" . rust-format-buffer)
+              ("C-c C-d" . helm-dash-at-point))
+  :init
+  (add-hook 'rust-mode-hook (lambda ()
+                              (rust/setup-dash)))
   :config
   (setq rust-format-on-save t
         rust-rustfmt-bin "rustfmt-force")
   :ensure t)
+
 
 (use-package flycheck-rust
   :pin "melpa"
