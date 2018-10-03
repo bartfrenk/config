cdcb1f6fafcabe8a8d3efcf2c4b988dfd20b39d1
Bart Frenk <bart.frenk@greenhousegroup.com>
2018-10-03 09:10:57 +0200

    Add neotreeo to Emacs

--- .emacs.d/init/init-core.el
+++ .emacs.d/init/init-core.el
@@ -33,6 +33,36 @@
 
 (use-package diminish)
 
+(use-package neotree
+  :commands (neotree-project-dir)
+  :config
+  (setq neo-window-width 40
+        neo-autorefresh nil)
+  (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+  (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+
+  (defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root.")))))
+
+(global-set-key [f8] `neotree-project-dir)
+
 (use-package ace-window
   :config
   (global-set-key (kbd "M-m") 'ace-window))
