5395f611051c6b139637f53cdc606260b07e0cc3
Bart Frenk <bart.frenk@gmail.com>
2018-10-21 21:28:33 +0200

    Update Emacs config

--- .emacs.d/init/init-python-pyenv.el
+++ .emacs.d/init/init-python-pyenv.el
@@ -2,6 +2,7 @@
 (require 'projectile)
 (require 'helm-buffers)
 (require 'helm-dash)
+(require 'flycheck)
 
 ;; ==== python-mode utility functions ====
 
@@ -139,6 +140,11 @@ working directory to the project base dir."
 (use-package python-docstring
   :pin "melpa"
   :diminish python-docstring-mode)
+
+(use-package flycheck-mypy
+  :config
+  (add-to-list 'flycheck-checkers 'python-mypy t)
+  (flycheck-add-next-checker 'python-pylint 'python-mypy t))
 
 (jedi:setup)
 
