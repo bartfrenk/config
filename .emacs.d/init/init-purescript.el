6567fa6d0429ba494da209c6fd49a726c886de1d
Bart Frenk <bart.frenk@gmail.com>
2018-11-02 23:46:44 +0100

    Upgrade purescript config

--- .emacs.d/init/init-purescript.el
+++ .emacs.d/init/init-purescript.el
@@ -19,16 +19,29 @@
     (message (concat "Starting server in " root))
     (psc-ide-server-start root)))
 
+(defun purescript/reformat ()
+  (interactive)
+  (purescript-mode-format-imports))
+
 (let ((root (string-join (reverse segments) "/")))
   (message (concat "Starting server in " root))
   (psc-ide-server-start root ))
+
+;; Attempt to get REPL support in Emacs (not yet working: see also
+;; https://github.com/purescript/purescript/issues/2886.
+(use-package psci
+  :config
+  (setq psci/file-path "pulp"
+        psci/arguments '("repl")))
+
 
 (use-package psc-ide
   :bind
   (:map purescript-mode-map
         ("M-]" . psc-ide-goto-definition)
         ("M-[" . xref-pop-marker-stack)
-        ("C-c M-j" . purescript/restart-server))
+        ("C-c M-j" . purescript/restart-server)
+        ("C-c C-a" . purescript/reformat))
   :commands (psc-ide-mode turn-on-purescript-indentation)
   :pin "melpa"
   :config
@@ -38,6 +51,7 @@
             (lambda ()
               (psc-ide-mode)
               (turn-on-purescript-indentation))))
+
 
 
 (provide 'init-purescript)
