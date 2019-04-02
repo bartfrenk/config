e155ebbd21c98b9a790eb5ba649c42b1f31746aa
Bart Frenk <bart.frenk@gmail.com>
2019-04-03 00:50:12 +0200

    Allow settings resolve-deps aliases by setting a defvar

--- .emacs.d/init/init-clojure.el
+++ .emacs.d/init/init-clojure.el
@@ -2,18 +2,17 @@
 (require 'evil)
 (require 'helm-buffers)
 (require 'clojure-mode)
+(require 'functions)
+(require 'cider)
 
 (defun clojure-reformat ()
   (interactive)
   (cider-format-buffer)
   (cljr-clean-ns))
 
-
 (use-package flycheck-joker)
 
-
 (use-package sesman)
-
 
 (use-package clojure-mode
   :commands clojure-mode
@@ -21,7 +20,6 @@
   (add-hook 'clojure-mode-hook (lambda ()
                                  (paredit-mode 1)
                                  (evil-paredit-mode 1)
-                                 ;;(flycheck-clojure-setup)
                                  (clj-refactor-mode 1)
                                  (yas-minor-mode 1)))
   :config
@@ -52,11 +50,27 @@
 
   ;;   )
 
-  (setq clojure-indent-style :always-align
-        clojure-align-forms-automatically t))
+)
 
+(defvar clojure/resolve-deps
+  '("test")
+  "Aliases in `deps.edn` for which to resolve dependencies when starting a REPL.")
 
+(defun clojure--cli-with-resolve-deps (fn &rest args)
+    "Prepends the resolve-deps aliases to the command line
+parameters for the clojure CLI and restores it to the old value
+when `fn` returns or fails."
+    (with-restore cider-clojure-cli-parameters
+      (if (null clojure/resolve-deps)
+          (apply fn args)
+        (let* ((prefix (->> clojure/resolve-deps
+                            (mapcar (lambda (s) (format "-R:%s" s)))
+                            (apply #'join)))
+               (tmp-val (join prefix cider-clojure-cli-parameters)))
+          (setq cider-clojure-cli-parameters tmp-val)
+          (apply fn args)))))
 
+(advice-add 'cider-jack-in-clj :around #'clojure--cli-with-resolve-deps)
 
 (use-package cider
   :bind (:map clojure-mode-map
@@ -69,10 +83,13 @@
                                (paredit-mode 1)))
   (add-hook 'cider-repl-mode-hook 'paredit-mode)
   :config
+
   (setq cider-prompt-for-symbol nil
         nrepl-hide-special-buffers t
         cider-use-overlays t
-        cider-repl-display-help-banner nil)
+        cider-repl-display-help-banner nil
+        cider-clojure-cli-parameters "-m nrepl.cmdline --middleware '%s'")
+
   (add-to-list 'evil-emacs-state-modes 'cider-stacktrace-mode)
   (add-to-list 'evil-emacs-state-modes 'cider-test-report-mode)
   (add-to-list 'evil-emacs-state-modes 'cider-docview-mode)
@@ -81,8 +98,7 @@
   :diminish cider-mode
   :ensure t)
 
-
-
-(use-package clj-refactor)
+(use-package clj-refactor
+  :diminish clj-refactor-mode)
 
 (provide 'init-clojure)
