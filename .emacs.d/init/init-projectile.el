2c7cc2ac47f12e09c07520d62c8f963f02b3734d
Bart Frenk <bart.frenk@gmail.com>
2019-05-25 12:51:09 +0200

    Add helm-projectile keybindings manually

--- .emacs.d/init/init-projectile.el
+++ .emacs.d/init/init-projectile.el
@@ -3,7 +3,6 @@
 (require 'helm)
 
 (use-package projectile
-  :ensure t
   :config
   (setq projectile-completion-system 'helm
         projectile-enable-caching t
@@ -16,10 +15,14 @@
 
 (use-package helm-projectile
   :commands (helm-projectile-on)
+  :pin "melpa"
   :config
   (setq projectile-switch-project-action 'helm-projectile)
   :bind (:map projectile-mode-map
-              ("C-c p s" . helm-projectile-ag)))
+              ("C-c p s" . helm-projectile-ag)
+              ("C-c p p" . helm-projectile-switch-project)
+              ("C-c p f" . helm-projectile-find-file)))
+
 
 (projectile-mode)
 (helm-projectile-on)
