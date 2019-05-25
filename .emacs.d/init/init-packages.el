d35ed655b88dcfdb83d05f0aee0967cd1f372b77
Bart Frenk <bart.frenk@gmail.com>
2019-05-25 12:52:15 +0200

    Update package URLs

--- .emacs.d/init/init-packages.el
+++ .emacs.d/init/init-packages.el
@@ -3,10 +3,11 @@
 (setq package-enable-at-startup nil)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")
-                         ("melpa-stable" . "http://stable.melpa.org/packages/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
 
 (package-initialize)
 
