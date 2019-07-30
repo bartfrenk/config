27f4435d9cf15d9f3ffc45455e7302bc6b4b586c
Bart Frenk <bart.frenk@gmail.com>
2019-07-30 23:01:53 +0200

    Add find-definition key bindings to Emacs Lisp mode

--- .emacs.d/init/init-elisp.el
+++ .emacs.d/init/init-elisp.el
@@ -5,4 +5,9 @@
                                   (paredit-mode 1)
                                   (evil-paredit-mode 1)))
 
+(define-key emacs-lisp-mode-map (kbd "M-]") 'xref-find-definitions)
+(define-key emacs-lisp-mode-map (kbd "M-[") 'xref-pop-marker-stack)
+
+(use-package emacs-lisp-mode)
+
 (provide 'init-elisp)
