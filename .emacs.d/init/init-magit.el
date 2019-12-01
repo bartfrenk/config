b13c0ccd7e164618be8eb2a96456722e2c392d6d
Bart Frenk <bart.frenk@gmail.com>
2019-12-01 23:45:33 +0100

    Update magit and friends to get forge working [emacs]

--- .emacs.d/init/init-magit.el
+++ .emacs.d/init/init-magit.el
@@ -1,10 +1,26 @@
 (use-package magit)
 
+
+
 (use-package magit-org-todos
   :config
   (magit-org-todos-autoinsert))
 
-(use-package evil-magit)
+;; To get forge working:
+;; https://www.reddit.com/r/emacs/comments/a7r265/work_with_git_forges_from_the_comfort_of_magit/
 
+;; For now, it seems that the only thing to get it working is to do `git config
+;; gitlab.user bartfrenk` in the repository you want to use forge with.
+
+(use-package forge
+  :pin melpa
+  :after magit)
+
+(use-package ghub
+  :pin melpa)
+
+(use-package evil-magit
+  :pin melpa
+  :after magit)
 
 (provide 'init-magit)
