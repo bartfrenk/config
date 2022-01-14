7405290331a444c00d8f4e68f24efbbf85bb7335
Bart Frenk <bart.frenk@gmail.com>
2022-01-14 15:22:43 +0100

    Remove emacs config from repo

--- .emacs.d/init/init-jinja2.el
+++
@@ -1,6 +0,0 @@
-(require 'use-package)
-
-(use-package jinja2-mode
-  :ensure t)
-
-(provide 'init-jinja2)
--- .emacs.d/init/init-solidity.el
+++
@@ -1,9 +0,0 @@
-(require 'use-package)
-
-(use-package solidity-mode)
-
-(use-package company-solidity
-  :after solidity-mode)
-
-(use-package solidity-flycheck
-  :after solidity-mode)
