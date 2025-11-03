ac40bfdee278a20a0ac7198090617214bc9f426f
Bart Frenk <bart.frenk@gmail.com>
2025-11-03 17:47:57 +0100

    Set up pyright LSP for Python mode in Emacs

--- doom/init.el
+++ doom/init.el
@@ -163,7 +163,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +pyright +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
