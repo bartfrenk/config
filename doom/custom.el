28fee272a16489d45fb77ad4b173b2c9703bffc1
Bart Frenk <bart.frenk@gmail.com>
2025-11-03 12:55:36 +0100

    Update custom.el

--- doom/custom.el
+++ doom/custom.el
@@ -4,7 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e" default)))
+   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e" default))
+ '(safe-local-variable-values
+   '((projectile-project-root . "~/.config/xmonad/")
+     (projectile-project-root . "/home/bart/.config/xmonad")
+     (projectile-project-root . "~/.config/xmonad")
+     (projectile-project-root . "$HOME/.config/xmonad")
+     (projectile-project-root . ".")
+     (setq projectile-project-root "."))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
