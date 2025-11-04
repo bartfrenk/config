33c650b5c995b2dd5ac9345e025f56e8bd9dd5ad
Bart Frenk <bart.frenk@gmail.com>
2025-11-04 11:48:50 +0100

    Add black autoformatting to Emacs Python config

--- doom/custom.el
+++ doom/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e" default))
+   '("3c6bd0ca16d4355d34c57e328fda29dbe69d91f260306662aa9c30cc08744e36" "44e6d6845e571402c149a12acbf698391e62928443b3d1924478bbc54a573b32" "b9f44212b4be6f0466811c5d8a297dda3c40dbf4c4cfd97c1686fceb2043b617" "d35afe834d1f808c2d5dc7137427832ccf99ad2d3d65d65f35cc5688404fdf30" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e" default))
+ '(package-selected-packages
+   '(zeno-theme inkpot-theme github-dark-vscode-theme vscode-dark-plus-theme python-black))
  '(safe-local-variable-values
    '((projectile-project-root . "~/.config/xmonad/")
      (projectile-project-root . "/home/bart/.config/xmonad")
