def226942330a73dc9d2a2264fee6d6a2213f57c
Bart Frenk <bart.frenk@gmail.com>
2025-11-24 13:16:29 +0100

    Add CPAN/PERL config to oh-my-zsh

---
+++ oh-my-zsh/perl.zsh
@@ -0,0 +1,5 @@
+PATH="/home/bart/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/home/bart/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/home/bart/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/home/bart/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/home/bart/perl5"; export PERL_MM_OPT;
