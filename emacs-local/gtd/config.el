73b3ae73dbeebff5cf001defea118e4d3bc6b5d4
Bart Frenk <bart.frenk@gmail.com>
2026-08-11 19:04:50 +0200

    Clean up GTD package

--- emacs-local/gtd/config.el
+++ emacs-local/gtd/config.el
@@ -2,38 +2,63 @@
 
 (defvar gtd/dir)
 
-(defun gtd/prune-capture-templates (key)
+(defun gtd--path (file)
+  (concat gtd/dir "/" file))
+
+(defun gtd/inbox ()
+  (interactive)
+  (find-file (gtd--path "inbox.org")))
+
+(defun gtd/projects ()
+  (interactive)
+  (find-file (gtd--path "projects.org")))
+
+(defun gtd/actions ()
+  (interactive)
+  (find-file (gtd--path "actions.org")))
+
+(defun gtd/font-lock-refresh ()
+  (interactive)
+  (progn
+    (font-lock-flush)
+    (font-lock-ensure)
+    (redisplay t)))
+
+(defun gtd--set-org-todo-keyword-faces ()
+  (setq
+   org-modern-todo nil
+   org-todo-keyword-faces
+   '(("TODO"      . (:inherit success :weight bold :underline t))
+     ("NEXT"      . (:inherit font-lock-keyword-face :weight bold :underline t))
+     ("URGENT"    . (:inherit error :weight bold :underline t))
+     ("WAITING"   . (:inherit warning :weight bold :underline t))
+     ("DONE"      . (:inherit shadow :weight bold :underline t))
+     ("CANCELLED" . (:inherit shadow :weight bold :underline t)))))
+
+(defun gtd--remove-capture-templates (key)
   (setq org-capture-templates
         (cl-remove-if
          (lambda (tpl)
            (string= (car tpl) key))
          org-capture-templates)))
 
-(defun gtd/path (file-name)
-  (concat gtd/dir "/" file-name))
-
-(defun gtd/add-capture-templates ()
+(defun gtd--set-capture-templates ()
+  (gtd--remove-capture-templates "i")
   (add-to-list 'org-capture-templates
                `("i" "Inbox" entry
-                 (file ,(gtd/path "inbox.org"))
+                 (file ,(gtd--path "inbox.org"))
                  "* TODO %?\n %U\n\n")))
 
-(defun gtd/inbox ()
-  (interactive)
-  (find-file (gtd/path "inbox.org")))
+(defun gtd--register-files ()
+  (setq org-refile-targets
+        `((,(gtd--path "projects.org") :maxlevel . 3)
+          (,(gtd--path "actions.org")  :maxlevel . 2))
+        org-refile-use-outline-path 'file
+        org-outline-path-complete-in-steps nil
+        org-refile-allow-creating-parent-nodes 'confirm
+        org-agenda-files (list (gtd--path "actions.org") (gtd--path "projects.org"))))
 
-(defun gtd/projects ()
-  (interactive)
-  (find-file (gtd/path "projects.org")))
-
-(defun gtd/actions ()
-  (interactive)
-  (find-file (gtd/path "actions.org")))
-
-(defun gtd/set-agenda-files ()
-  (setq org-agenda-files (list (gtd/path "actions.org") (gtd/path "projects.org"))))
-
-(defun my/org-agenda-top-parent ()
+(defun gtd--org-agenda-top-parent ()
   (save-excursion
     (org-back-to-heading t)
     (let ((label (if (org-up-heading-safe)
@@ -42,25 +67,31 @@
                        (org-get-heading t t t t)))))
       (format "%-20s" (or label "")))))
 
+(defun gtd--set-agenda-format ()
+  (setq org-agenda-prefix-format
+        '((agenda . " %i %-12:c %?-12t %b ")
+          (todo   . " %(gtd--org-agenda-top-parent)")
+          (tags   . " %i %-12:c %b ")
+          (search . " %(gtd--org-agenda-top-parent)"))))
 
-(setq org-agenda-prefix-format
-      '((agenda . " %i %-12:c %?-12t %b ")
-        (todo   . " %(my/org-agenda-top-parent)")
-        (tags   . " %i %-12:c %b ")
-        (search . " %i %-12:c %b ")))
+(defun gtd--set-keybindings ()
+  (map! :leader
+        :desc "Open inbox"
+        "o i" #'gtd/inbox)
+  (map! :leader
+        :desc "Open inbox"
+        "n g i" #'gtd/inbox)
+  (map! :leader
+        :desc "Open projects"
+        "n g p" #'gtd/projects)
+  (map! :leader
+        :desc "Open actions"
+        "n g a" #'gtd/actions))
 
-(defun gtd/init ()
-  (gtd/prune-capture-templates "t")
-  (gtd/prune-capture-templates "i")
-  (gtd/add-capture-templates)
-  (gtd/set-agenda-files)
-
-  (setq org-refile-targets
-        `((,(gtd/path "projects.org") :maxlevel . 3)
-          (,(gtd/path "actions.org")  :maxlevel . 2)))
-
-  (setq org-refile-use-outline-path 'file)
-  (setq org-outline-path-complete-in-steps nil)
-  (setq org-refile-allow-creating-parent-nodes 'confirm))
-
-(gtd/init)
+(defun gtd/init (dir)
+  (setq gtd/dir dir)
+  (gtd--set-capture-templates)
+  (gtd--set-org-todo-keyword-faces)
+  (gtd--register-files)
+  (gtd--set-agenda-format)
+  (gtd--set-keybindings))
